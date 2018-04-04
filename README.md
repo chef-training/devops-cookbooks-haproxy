b8022ecd9a2f81f05d82b72990256875e79eba32
SamMc87 <sam@technotrainer.com>
2018-04-04 14:22:21 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# devops-cookbooks-haproxy
+This repo contains the various versions of the 'myhaproxy' cookbook associated with the DevOps Foundation class.
---
+++ myhaproxy-version_0.2.0/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: myhaproxy
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+	  'hostname' => 'cloud.public_hostname',
+	  'ipaddress' => 'cloud.public_ipv4',
+	  'port' => 80,
+	  'ssl_port' => 80
+	},{
+	  'hostname' => 'cloud.public_hostname',
+	  'ipaddress' => 'cloud.public_ipv4',
+	  'port' => 80,
+	  'ssl_port' => 80
+	}]
+	
+include_recipe 'haproxy::manual'
